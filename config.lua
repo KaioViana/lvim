e9a389e7e8647e1ad095e999626c38ee4b7863de
kaioViana <kaiu.viana@hotmail.com>
2023-04-15 17:03:31 -0300

    adjust initial keys maps

--- config.lua
+++ config.lua
@@ -1,14 +1,9 @@
-reload("typescript.linters")
-reload("typescript.code_actions")
-
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = false
 lvim.colorscheme = "lunar"
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
--- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
@@ -33,3 +28,7 @@ lvim.builtin.treesitter.ensure_installed = {
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
 lvim.format_on_save = true
+
+reload("typescript.code_actions")
+reload("typescript.linters")
+reload("common.keys_map")
---
+++ lua/common/keys_map.lua
@@ -0,0 +1,7 @@
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["<S-w>"] = ":bd<CR>"
+lvim.keys.normal_mode["|"] = ":vsplit<CR>"
+lvim.keys.normal_mode["-"] = ":split<CR>"
+lvim.keys.normal_mode["<C-t>"] = ":ToggleTerm<CR>"
